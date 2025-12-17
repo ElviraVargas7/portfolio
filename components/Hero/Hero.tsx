f2c30b56903f3896112e38044e65b8c35983ae18
ElviraVargas7 <elviravarber@gmail.com>
2025-12-17 12:02:52 -0600

    addin missing responsiveness to hero

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -3,13 +3,13 @@ import Image from "next/image";
 const Hero = () => {
   return (
     <div className="w-full">
-      <div className="flex flex-row items-center gap-6 text-center sm:items-start sm:text-left">
-        <div className="flex flex-col gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-3 items-center gap-6 text-center sm:items-start sm:text-left">
+        <div className="flex flex-col col-span-2 gap-4">
           <div className="border border-solid border-[#c7417b] rounded-3xl bg-[#F5D6E1] dark:bg-[#E31C8D] w-fit px-4 py-2 mb-2">
             <p className="text-[#c7417b] dark:text-[#c7417b]">Hello, I'm</p>
           </div>
 
-          <h1 className="font-semibold leading-10 tracking-tight text-6xl text-black dark:text-[#fce7f3]">
+          <h1 className="font-semibold leading-10 tracking-tight text-6xl text-black dark:text-[#fce7f3] mb-1">
             Elvira Vargas Bermudez
           </h1>
 
@@ -62,7 +62,9 @@ const Hero = () => {
           </div>
         </div>
 
-        <Image src="/me.png" alt="Me" width={300} height={20} priority />
+        <div className="flex justify-center col-span-1">
+          <Image src="/me.png" alt="Me" width={300} height={20} priority />
+        </div>
       </div>
 
       <div className="flex justify-center">
