55fa4a35ae37cec0455e7003b3ee32b291b39f83
ElviraVargas7 <elviravarber@gmail.com>
2025-12-17 22:34:33 -0600

    adding basic footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,24 @@
 import AboutMe from "@/components/AboutMe/AboutMe";
 import Contact from "@/components/Contact/Contact";
+import Footer from "@/components/Footer/Footer";
 import Hero from "@/components/Hero/Hero";
 import Projects from "@/components/Projects/Projects";
 import SkillsTechStack from "@/components/SkillsTechStack/SkillsTechStack";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-[#fce7f3] dark:bg-[#400827] font-sans">
-      <main className="flex gap-28 min-h-screen w-full max-w-6xl flex-col items-center justify-between py-32 px-16 bg-[#fce7f3] dark:bg-[#400827] sm:items-start">
+    <div className="min-h-screen bg-[#fce7f3] dark:bg-[#400827] font-sans">
+      <main className="mx-auto flex max-w-6xl flex-col items-center gap-28 py-32 px-16 sm:items-start">
         <Hero />
         <AboutMe />
         <SkillsTechStack />
         <Projects />
         <Contact />
       </main>
+
+      <footer className="w-full">
+        <Footer />
+      </footer>
     </div>
   );
 }
